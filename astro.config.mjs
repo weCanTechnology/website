9fc38f32623a2a8414dfb58975aab719e32bd370
Melinda Darvasi <melinda.darvasi@brightcove.com>
2026-01-24 17:05:26 +0100

    update config to https://wecan.technology

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://wecantechnology.github.io/website',
-    base: '/website',
+    site: 'https://wecan.technology',
+    base: '/', 
 });
-0
